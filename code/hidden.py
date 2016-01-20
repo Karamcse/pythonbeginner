a2f58f00ef22ffde10d125e374a736fa5bfb9c61
G Karamchand <karamchand.g@karvy.com>
2016-01-20 11:22:14 +0530

    Code and slide

---
+++ code/hidden.py
@@ -0,0 +1,7 @@
+# Keep this file separate
+
+def oauth() :
+    return { "consumer_key" : "8nGiBTEaZQ9wYlcXonD80GyaK",
+        "consumer_secret" : "tPSTUnHqFLLLtpXVIv7ZMH0x4RoXgHhT6FtV7YqCgvk9GWy4ej",
+        "token_key" : "215665928-IEJ3KCbG4NX1xfb0dE2M2qvgbWSTmLj15ZRfO5TI",
+        "token_secret" : "kqunLoUGTTvcpdzKriIwMbSsK3M52dA0rdsawRGrxU5dP" }
